20e3acd063dd139a56bc723cfefe60490220e799
Aquilexus <paulsoual@gmail.com>
2022-09-16 17:31:05 +0200

    Main Se empieza a trabajar sobre el hall of fame

--- code/hallOfFame.php
+++ code/hallOfFame.php
@@ -1,3 +1,19 @@
-<?php
-echo(test);
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="score.css">
+    </head>
+    <body>
+        <div class="top">
+            <div class= "logo">
+                <img src="../images/fame_title.png" alt="" srcset="">
+            </div>
+        </div>
+        <div class="middle">
+            <div class="board">
+                <p>test</p>
+
+            </div>
+        </div>
+    </body>
+</html>
